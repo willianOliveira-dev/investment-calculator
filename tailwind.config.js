e3ee8ee9a2aafe38697dd5963e5f8abe1ce63ecb
devWillian <willian.dev.tech@gmail.com>
2025-05-24 11:11:46 -0300

    refactor: Extende mais opções de cores nas configurações do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rich: {
+          bg: "#101d25", 
+          neon: "#00ffae", 
+          gold: "#ffd54f", 
+          hoverGold: "#ffca28", 
+          input: "#0d141a", 
+          text: "#ffffff", 
+          soft: "#aaaaaa", 
+          shadow: "rgba(0, 255, 174, 0.3)", 
+        },
+      },
+    },
   },
   plugins: [],
 };
